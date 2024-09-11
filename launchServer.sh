3110c95ad75650b0b9bc0b1b92f70b1dccbdece9
cat <yzu4i1z5@anonaddy.me>
2024-09-11 06:44:00 +0100

    .

--- launchServer.sh
+++ launchServer.sh
@@ -1,2 +1,4 @@
-python3 -m http.server
-
+cd /home/agl/arcadeDir/
+git pull
+python3 -m http.server -d /home/agl/arcadeDir/ &
+chromium-browser --start-fullscreen --incognito --disable-session-crashed-bubble --disable-infobars http://localhost:8000
