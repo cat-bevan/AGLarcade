0e4fc03a179f8bd90d10a998c1e331cfc5f51d01
cat <yzu4i1z5@anonaddy.me>
2024-09-11 07:52:10 +0100

    boop

--- launchBrowser.sh
+++ launchBrowser.sh
@@ -1,4 +1,4 @@
-cd /home/agl/arcadeDir/
-git pull
+sleep 3
+sudo -u agl git --git-dir=/home/agl/arcadeDir/.git pull
 python3 -m http.server -d /home/agl/arcadeDir/ &
 chromium-browser --start-fullscreen --incognito --disable-session-crashed-bubble --disable-infobars http://localhost:8000
