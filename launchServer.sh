8e32f4e80397594ede55b47558ca2a562cd2ee11
cat-bevan <70996599+cat-bevan@users.noreply.github.com>
2024-09-16 22:22:26 -0600

    Update launchServer.sh

--- launchServer.sh
+++ launchServer.sh
@@ -1,4 +1,3 @@
 cd /home/agl/arcadeDir/
 git pull
-python3 -m http.server -d /home/agl/arcadeDir/ &
 chromium-browser --start-fullscreen --incognito --disable-session-crashed-bubble --disable-infobars https://cat-bevan.github.io/AGLarcade/
